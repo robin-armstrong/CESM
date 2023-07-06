ac02296d325c84729ae5a35df5bf6bc7631f0ea7
Robin Armstrong <rarmstrong@ucar.edu>
2023-07-06 10:54:43 -0600

    Further modifications to prepare for parallelization.

--- standalone/examples/single_column_MARBL/BATS/ensemble/baseline/README.md
+++ standalone/examples/single_column_MARBL/BATS/ensemble/baseline/README.md
@@ -1,6 +1,4 @@
-This folder contains the necessary data to reset the MOM6-BATS simulation in preparation for
-re-running the assimilation loop. As such, its contents should not be modified except to generate
-or re-generate the files in the RESTART sub-folder. This can be accomplished by running
+This folder contains the necessary data to reset the ensemble members in preparation for running MOM6+MARBL with DART. As such, its contents should not be modified except to generate or re-generate the files in the RESTART sub-folder. This can be accomplished by running
 
 ``../../../../../build/intel/MOM6/MOM6``
 
--- standalone/examples/single_column_MARBL/BATS/run_with_dart.sh
+++ standalone/examples/single_column_MARBL/BATS/run_with_dart.sh
@@ -2,21 +2,20 @@
 
 # ======================= SCRIPT PARAMETERS =======================
 
-# important filesystem paths
-MOM6_BIN=../../../build/intel/MOM6/MOM6                            # the MOM6 binary
-MOM6_WORK_DIR=./                                                   # working directory for MOM6
-DART_MODEL_DIR=../../../../../../../../DART/models/MARBL_MOM6_1D   # location of DART model information
-ENSEMBLE_DIR=$(pwd)/RESTART/DART_ensemble                          # directory containing restart files
-BASE_RES_FILE=$(pwd)/RESTART/DART_baseline_restart/MOM.res.nc      # baseline restart file to initialize ensemble
+# MOM6 binary, keep this path absolute
+MOM6_BIN=~/work/cesm/cesm2_3_alpha12b+mom6_marbl/components/mom/standalone/build/intel/MOM6/MOM6
+
+# important filesystem paths, keep these absolute
+MOM6_BATS_DIR=$(pwd)                                               # working directory for MOM6
+DART_MODEL_DIR=~/work/DART/models/MARBL_MOM6_1D                    # location of DART model information
 OBSSEQ_DIR=~/work/BATS_obsseq                                      # location of obs-sequence files
 
 # DA config options
-ENS_SIZE=15
+ENS_SIZE=5
 
 # other
-MOM_RES_NAME=MOM.res.nc         # generic restart file name
-SOLO_RES_NAME=ocean_solo.res    # seems like there are important files with this name?
 MOM6_TO_DART=139157             # offset between MOM6 and DART calendars
+MOM6_TIMESTEP=100.0             # timestep to use when advancing ensemble members
 
 # ======================= MAIN PROGRAM =======================
 
@@ -26,7 +25,7 @@ echo "============================ SCRIPT ============================"
 echo "================================================================"
 echo ""
 
-echo "old restart files and analysis obs-sequence files will be deleted."
+echo "old ensemble data and analysis obs-sequence files will be deleted."
 read -p "continue? [y/n] " userchoice
 
 if [ "${userchoice}" != "y" ] && [ "${userchoice}" != "Y" ]
@@ -37,9 +36,9 @@ then
     exit
 fi
 
-echo "deleting old restart files..."
+echo "deleting old ensemble data files..."
 
-rm -rf ${ENSEMBLE_DIR}/*
+rm -rf ${MOM6_BATS_DIR}/ensemble/member_*
 
 echo "deleting old analysis obs-sequence files..."
 
@@ -50,18 +49,34 @@ echo "resetting the restart file list..."
 truncate -s 0 ${DART_MODEL_DIR}/work/ensemble_members.txt
 
 for i in $(seq ${ENS_SIZE}); do
-    ensemble_subdir=${ENSEMBLE_DIR}/member_$(printf "%04d" ${i})
+    ensemble_subdir=${MOM6_BATS_DIR}/ensemble/member_$(printf "%04d" ${i})
     mkdir ${ensemble_subdir}
-    echo "${ensemble_subdir}/${MOM_RES_NAME}" >> ${DART_MODEL_DIR}/work/ensemble_members.txt
+    echo "${ensemble_subdir}/RESTART/MOM.res.nc" >> ${DART_MODEL_DIR}/work/ensemble_members.txt
 done
 
-echo "resetting the initial restart file..."
+echo "initializing the ensemble members..."
 
-cp ${BASE_RES_FILE} ${ENSEMBLE_DIR}/member_0001/${MOM_RES_NAME}
+for i in $(seq ${ENS_SIZE}); do
+    ensemble_subdir=${MOM6_BATS_DIR}/ensemble/member_$(printf "%04d" ${i})
+    baseline_subdir=${MOM6_BATS_DIR}/ensemble/baseline
+
+    cp -r ${baseline_subdir}/RESTART ${ensemble_subdir}/RESTART
+    cp -r ${baseline_subdir}/INPUT ${ensemble_subdir}/INPUT
+    cp ${baseline_subdir}/MOM_input ${ensemble_subdir}/MOM_input
+    cp -r ${baseline_subdir}/MOM_override ${ensemble_subdir}/MOM_override
+    cp ${baseline_subdir}/MOM_override2 ${ensemble_subdir}/MOM_override2
+    cp ${baseline_subdir}/data_table ${ensemble_subdir}/data_table
+    cp ${baseline_subdir}/diag_table ${ensemble_subdir}/diag_table
+    cp ${baseline_subdir}/input.nml ${ensemble_subdir}/input.nml
+
+    sed -i "s/input_filename = .*/input_filename = 'r',/" ${ensemble_subdir}/input.nml
+    sed -i "s%restart_input_dir = .*%restart_input_dir = 'RESTART/',%" ${ensemble_subdir}/input.nml
+    sed -i "s/DT = .*/DT = ${MOM6_TIMESTEP}/" ${ensemble_subdir}/MOM_input
+done
 
 echo "determining the initial model time..."
 
-timestr=$(ncdump ${BASE_RES_FILE} | grep "Time = [0123456789]* ;")
+timestr=$(ncdump ${MOM6_BATS_DIR}/ensemble/baseline/RESTART/MOM.res.nc | grep "Time = [0123456789]* ;")
 timestr_length=${#timestr}
 let timestr_lastindex=${timestr_length}-2
 let timestamp_length=${timestr_lastindex}-8
@@ -91,8 +106,6 @@ fi
 echo "will simulate until day ${lastday_dart} (DART calendar),"
 echo "                        ${lastday_mom6} (MOM6 calendar)."
 
-exit
-
 echo "setting perturb_from_single_instance = .true. ..."
 sed -i "s/perturb_from_single_instance = .*/perturb_from_single_instance = .true./" ${DART_MODEL_DIR}/work/input.nml
 
@@ -106,23 +119,7 @@ do
 
     if [ -f "${OBSSEQ_DIR}/out/BATS_${currentday_dart}.out" ]
     then
-        if ! ${first_assimilation_complete}
-        then
-            echo "found file."
-            echo "preparing restart files for ensemble members 2 through ${ENS_SIZE}..."
-
-            for i in $(seq ${ENS_SIZE}); do
-                if [ 2 -le ${i} ]
-                then
-                    cp ${ENSEMBLE_DIR}/member_0001/${MOM_RES_NAME} ${ENSEMBLE_DIR}/member_$(printf "%04d" ${i})/${MOM_RES_NAME}
-                    cp ${ENSEMBLE_DIR}/member_0001/${SOLO_RES_NAME} ${ENSEMBLE_DIR}/member_$(printf "%04d" ${i})/${SOLO_RES_NAME}
-                fi
-            done
-
-            echo "assimilating obs-sequence file with DART..."
-        else
-            echo "found file, a/ssimilating with DART..."
-        fi
+        echo "found file, assimilating with DART..."
 
         sed -i "s%obs_sequence_in_name.*%obs_sequence_in_name         ='\\${OBSSEQ_DIR}/out/BATS_${currentday_dart}.out',%" ${DART_MODEL_DIR}/work/input.nml
         sed -i "s%obs_sequence_out_name.*%obs_sequence_out_name        ='\\${OBSSEQ_DIR}/final/BATS_${currentday_dart}.final',%" ${DART_MODEL_DIR}/work/input.nml
@@ -146,13 +143,13 @@ do
 
         cd ${back}
 
-        grep -q "FATAL" program_output.txt
+        grep -q "ERROR" program_output.txt
         status=${?}
         rm program_output.txt
 
         if [ ${status} -eq 0 ]
         then
-            echo "fatal error detected from DART."
+            echo "error detected from DART."
             echo "exiting..."
             echo ""
             exit
@@ -180,11 +177,9 @@ do
         echo "advancing the model to day ${tomorrow_mom6} (MOM6 calendar)..."
         echo "                           ${tomorrow_dart} (DART calendar)..."
 
-        sed -i "s/DAYMAX = [0123456789]*/DAYMAX = ${tomorrow_mom6}/" ${MOM6_WORK_DIR}/MOM_input
-        sed -i "s%restart_input_dir = .*%restart_input_dir = '${ENSEMBLE_DIR}/member_0001',%" ${MOM6_WORK_DIR}/input.nml
-        sed -i "s%restart_output_dir = .*%restart_output_dir = '${ENSEMBLE_DIR}/member_0001',%" ${MOM6_WORK_DIR}/input.nml
+        sed -i "s/DAYMAX = [0123456789]*/DAYMAX = ${tomorrow_mom6}/" ${MOM6_BATS_DIR}/ensemble/member_0001/MOM_input
         back=$(pwd)
-        cd ${MOM6_WORK_DIR}
+        cd ${MOM6_BATS_DIR}/ensemble/member_0001
 
         echo ""
         echo "================================================================"
@@ -218,16 +213,15 @@ do
     else
         echo "advancing the ensemble to day ${tomorrow_mom6} (MOM6 calendar)..."
         echo "                              ${tomorrow_dart} (DART calendar)..."
-
-        sed -i "s/DAYMAX = [0123456789]*/DAYMAX = ${tomorrow_mom6}/" ${MOM6_WORK_DIR}/MOM_input
         
         for i in $(seq ${ENS_SIZE}); do
-            sed -i "s%restart_input_dir = .*%restart_input_dir = '${ENSEMBLE_DIR}/member_$(printf "%04d" $i)',%" ${MOM6_WORK_DIR}/input.nml
-            sed -i "s%restart_output_dir = .*%restart_output_dir = '${ENSEMBLE_DIR}/member_$(printf "%04d" $i)',%" ${MOM6_WORK_DIR}/input.nml
+            member_subdir=${MOM6_BATS_DIR}/ensemble/member_$(printf "%04d" ${i})
+            
+            sed -i "s/DAYMAX = .*/DAYMAX = ${tomorrow_mom6}/" ${member_subdir}/MOM_input
             
             echo "advancing ensemble member ${i}..."
             back=$(pwd)
-            cd ${MOM6_WORK_DIR}
+            cd ${member_subdir}
 
             echo ""
             echo "================================================================"
