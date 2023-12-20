b732fe0818064af71986062e1e83107d622f7f5f
Robin Armstrong <rarmstrong@ucar.edu>
2023-12-20 15:10:28 -0700

    Added code for MARBL-DART state estimation (developed on marblmom61d_dart branch) and MARBL-DART joint state-parameter estimation (developed on param_estimation branch).

---
+++ standalone/examples/single_column_MARBL/BATS_joint_estimation/marbl_to_dart.py
@@ -0,0 +1,51 @@
+import sys
+import re
+import numpy as np
+import netCDF4 as nc
+
+################### SCRIPT PARAMETERS ###################
+
+paramlist = ["autotroph_settings(1)%kDOP",
+             "autotroph_settings(1)%kNH4"]
+             
+numlayers = 65
+
+################### MAIN PROGRAM ########################
+
+marbl_in  = open(sys.argv[1], "r")
+timestamp = sys.argv[2]
+dart_out  = nc.Dataset(sys.argv[3], "w")
+
+# setting up the NetCDF file
+layer      = dart_out.createDimension("Layer", size = numlayers)
+timedim    = dart_out.createDimension("Time", size = 1)
+timevar    = dart_out.createVariable("Time", 'i8', ("Time",))
+timevar[0] = timestamp
+
+ncparams   = [None]*len(paramlist)
+
+for i in range(len(paramlist)):
+    ncparams[i] = dart_out.createVariable(paramlist[i], 'f8', ("Layer",))
+
+# regular expressions to extract parameter name and value from text
+pname_regex = re.compile(r'^[^\s]+')
+pval_regex  = re.compile(r'[^\s]+$')
+
+# populating the NetCDF file
+for line in marbl_in.readlines():
+    pname_array = re.findall(pname_regex, line)
+
+    if(len(pname_array) == 0):  # skips empty lines
+        continue
+    
+    pname = pname_array[0]
+    
+    for i in range(len(paramlist)):
+        if(pname == paramlist[i]):
+            pval_array = re.findall(pval_regex, line)
+            pval       = float(pval_array[0])
+            
+            ncparams[i][:] = pval*np.ones(numlayers)
+
+marbl_in.close()
+dart_out.close()
---
+++ standalone/examples/single_column_MARBL/BATS_state_estimation/ensemble/baseline/README.md
@@ -0,0 +1,6 @@
+This folder contains the necessary data to reset the ensemble members in preparation for running MOM6+MARBL with DART. As such, its contents should not be modified except to generate or re-generate the files in the RESTART sub-folder. This can be accomplished by running
+
+``../../../../../build/intel/MOM6/MOM6``
+
+from within the present folder.
+
